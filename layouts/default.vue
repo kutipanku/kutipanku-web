97b97689549c84b8f73efacb8a4608b109525e0d
trastanechora <trastanechora@gmail.com>
2021-02-12 07:43:46 +0700

    Setup layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" fixed app>
+    <v-navigation-drawer :mini-variant="miniVariant" app permanent>
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in drawerNavMenu"
           :key="i"
           :to="item.to"
           router
@@ -17,71 +17,54 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template #append>
+        <v-list>
+          <v-list-item router exact @click.stop="miniVariant = !miniVariant">
+            <v-list-item-action>
+              <v-icon
+                >mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon
+              >
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="'Lipat Menu'" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </template>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
+    <v-main class="background-canvas">
+      <v-layout justify-center>
+        <v-flex class="content-wrapper">
+          <v-container class="pa-0">
+            <nuxt />
+          </v-container>
+        </v-flex>
+      </v-layout>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    };
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import { MenuItem } from '../@types';
+
+@Component
+export default class Default extends Vue {
+  miniVariant: boolean = false;
+  clipped: boolean = false;
+  fixed: boolean = false;
+  drawer: boolean = true;
+
+  /* ------------------------------------
+  => Methods
+  ------------------------------------ */
+
+  /* ------------------------------------
+  => Setter and Getter
+  ** (Adopt store variables to local state)
+  ------------------------------------ */
+  get drawerNavMenu(): MenuItem {
+    return this.$store.state.ui.drawerNavMenu;
   }
-};
+}
 </script>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,42 @@
 <template>
-  <v-app dark>
+  <v-app dark class="text-center error-wrapper">
     <h1 v-if="error.statusCode === 404">
       {{ pageNotFound }}
     </h1>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <NuxtLink to="/"> Home page </NuxtLink>
   </v-app>
 </template>
 
-<script>
-export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head() {
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'error',
+  head(this: Error) {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
       title
-    }
+    };
   }
+})
+export default class Error extends Vue {
+  pageNotFound: string = '404 Halaman Tidak Ditemukan';
+  otherError: string = 'An error occurred';
+
+  @Prop()
+  error: any;
 }
 </script>
 
 <style scoped>
 h1 {
   font-size: 20px;
+}
+.error-wrapper {
+  padding-top: 200px;
 }
 </style>
