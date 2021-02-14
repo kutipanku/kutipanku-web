b5e1d9e15200619fd819273312d99993f0115ab0
trastanechora <trastanechora@gmail.com>
2021-02-14 11:37:18 +0700

    Update test

--- test/layouts/default.spec.ts
+++ test/layouts/default.spec.ts
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { state, actions, mutations } from '~/store/ui';
+
 import Default from '~/layouts/default.vue';
 
 Vue.use(Vuex);
@@ -13,6 +15,20 @@ app.setAttribute('data-app', 'true');
 document.body.appendChild(app);
 
 describe('Default layout unit test', () => {
+  // Create vuex store
+  let store: any;
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        ui: {
+          state,
+          actions,
+          mutations
+        }
+      }
+    });
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -21,6 +37,7 @@ describe('Default layout unit test', () => {
   test('is instantiated', () => {
     const wrapper = shallowMount(Default, {
       vuetify: new Vuetify(),
+      store,
       stubs: ['nuxt']
     });
 
