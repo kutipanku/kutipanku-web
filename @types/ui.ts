759ab5236a370a75990d306d8c4ea3e587450201
trastanechora <trastanechora@gmail.com>
2021-02-14 11:08:41 +0700

    Update typings

--- @types/ui.ts
+++ @types/ui.ts
@@ -4,6 +4,12 @@ export interface MenuItem {
   to?: string;
 }
 
+export interface Quote {
+  id: string;
+  content: string;
+  author: string;
+  category: string;
+}
 export interface UiState {
   drawerNavMenu: MenuItem[];
 }
