03a102bfec76067036cea3212f25a85dc3c7fad8
trastanechora <trastanechora@gmail.com>
2021-02-23 21:34:00 +0700

    Update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app :dark="!$vuetify.theme.dark" :light="$vuetify.theme.dark">
     <v-navigation-drawer :mini-variant="miniVariant" app permanent touchless>
       <v-list>
         <v-list-item class="px-2">
@@ -44,7 +44,14 @@
         </v-list>
       </template>
     </v-navigation-drawer>
-    <v-main class="background-canvas">
+    <v-main
+      class="background-canvas"
+      :style="{
+        background: $vuetify.theme.dark
+          ? $vuetify.theme.themes.dark.background
+          : $vuetify.theme.themes.light.background
+      }"
+    >
       <v-layout justify-center class="full-height">
         <v-flex class="content-wrapper">
           <v-container class="pa-0 full-height">
@@ -64,13 +71,22 @@ import KutipankuLogo from '../components/svg/KutipankuLogo.vue';
 @Component({
   components: {
     KutipankuLogo
-  }
+  },
+  middleware: ['themeInit']
 })
 export default class Default extends Vue {
   miniVariant: boolean = true;
+
+  created(): void {
+    this.initTheme();
+  }
+
   /* ------------------------------------
   => Methods
   ------------------------------------ */
+  initTheme(): void {
+    this.$vuetify.theme.dark = this.$store.state.ui.darkMode;
+  }
 
   /* ------------------------------------
   => Setter and Getter
