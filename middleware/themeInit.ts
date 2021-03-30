a69f94294d152a63733e6ce5d0e52b4bb644ce12
trastanechora <trastanechora@gmail.com>
2021-03-30 23:51:47 +0700

    Add console to test subdomain

--- middleware/themeInit.ts
+++ middleware/themeInit.ts
@@ -2,6 +2,7 @@ import { Middleware } from '@nuxt/types';
 
 const themeInit: Middleware = (context) => {
   if (process.server) {
+    console.warn('check subdomain:', context.req.rawHeaders[1].split('.')[0]);
     const darkMode = context.app.$cookies.get('darkMode');
     context.store.dispatch('ui/changeDarkMode', darkMode);
     const quoteLanguage = context.app.$cookies.get('quoteLanguage');
